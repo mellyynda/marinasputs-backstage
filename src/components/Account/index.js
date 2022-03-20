d721d020e35f1f33e1b45bee8cab6ac5c74eb7ad
Melinda Sava <itmaster@SESTOL00392.usr.ingenico.loc>
2022-03-20 11:55:55 +0100

    first commit

---
+++ src/components/Account/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+import { withFirebase } from '../Firebase';
+import { AuthUserContext, withAuthorization } from '../Session';
+
+
+const AccountPage = () => (
+    <AuthUserContext.Consumer>
+        {authUser => (
+            <div>
+                <h1>Account: {authUser.email}</h1>
+                <PasswordForgetForm />
+                <PasswordChangeForm />
+            </div>
+        )}
+    </AuthUserContext.Consumer>
+);
+
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
